f2804937eb2009328c22de755e054f259bf163a0
ruiting <tuanzhang@gmail.com>
2016-04-13 23:44:16 +0800

    add good logging

---
+++ server.js
@@ -0,0 +1,50 @@
+/**
+ * Created by tuanzhang on 13/4/2016.
+ */
+
+'use strict';
+
+const Hapi = require('hapi');
+const Good = require('good');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, reply) {
+
+        return reply('hello world');
+    }
+});
+
+// Start the server
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+    server.start((err) => {
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
